18560d9dea4e3adf3f4ababc388bb186e1355433
Hari Prasanth <hari16205@gmail.com>
2024-04-24 15:39:30 +0530

    Hosting issue

--- src/components/MeetingInfo/MeetingInfo.jsx
+++ src/components/MeetingInfo/MeetingInfo.jsx
@@ -1,63 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./MeetingInfo.css";
 import MeetingCard from "./MeetingCard/MeetingCard";
-const detailsArray = [
-  {
-    level1: "7/10",
-    level2: "6/10",
-    level3: "Waiting",
-    meetvia: "G-Meet",
-    interviewer1: "Stella",
-    interviewer2: "Smith",
-    interviewer3: "Stevan",
-    attendees: "Johnson",
-    date: "19 th Feb 2024",
-    time: "10:30 AM",
-    name: "John Smith",
-    role: "Python Developer",
-  },
-  {
-    level1: "7/10",
-    level2: "6/10",
-    level3: "Waiting",
-    meetvia: "G-Meet",
-    interviewer1: "Stella",
-    interviewer2: "Smith",
-    interviewer3: "Stevan",
-    attendees: "Johnson",
-    date: "19 th Feb 2024",
-    time: "10:30 AM",
-    name: "John Doe",
-    role: "Python Developer",
-  },
-  {
-    level1: "7/10",
-    level2: "6/10",
-    level3: "Waiting",
-    meetvia: "G-Meet",
-    interviewer1: "Stella",
-    interviewer2: "Smith",
-    interviewer3: "Stevan",
-    attendees: "Johnson",
-    date: "19 th Feb 2024",
-    time: "10:30 AM",
-    name: "John Been",
-    role: "Python Developer",
-  },
-];
+
 const MeetingInfo = () => {
+  const [detailsArray, setDetailsArray] = useState([]);
+  useEffect(() => {
+    fetch("./data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setDetailsArray(data.details);
+        console.log(data);
+      });
+  }, []);
   return (
     <div className="meetingInfo-container">
       <p className="meetingInfo-title">Today Interviews Meetings Info</p>
       <hr />
       <div className="meetingInfo-card">
-        {detailsArray.map((details, index) => {
-          return (
-            <div key={index}>
-              <MeetingCard details={details} />
-            </div>
-          );
-        })}
+        {detailsArray.length === 0 ? (
+          <p>loading</p>
+        ) : (
+          <>
+            {detailsArray.map((details, index) => {
+              return (
+                <div key={index}>
+                  <MeetingCard details={details} />
+                </div>
+              );
+            })}
+          </>
+        )}
       </div>
     </div>
   );
